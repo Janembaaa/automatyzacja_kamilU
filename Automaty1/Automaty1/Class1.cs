9847f790d42b4cb626b1767627be2e437b170a60
Kamil U <kamil.ubysz@intive.com>
2017-11-28 10:34:33 +0100

    Pierwszy commit

---
+++ Automaty1/Automaty1/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automaty1
+{
+    public class Class1
+    {
+    }
+}
