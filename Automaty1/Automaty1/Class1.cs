34d2911e5acb4ee41acb475048d635998813c44e
Kamil U <kamil.ubysz@intive.com>
2017-11-28 11:25:47 +0100

    First code + Xunit packages

--- Automaty1/Automaty1/Class1.cs
+++ Automaty1/Automaty1/Class1.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace Automaty1
 {
-    public class Class1
+    public class Mathematics
     {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Substract(double x, double y)
+        {
+
+            return x - y;
+        }
+
+        public double Multiply(double x, double y)
+        {
+
+            return x * y;
+        }
+
+        public double Divide(double x, double y)
+        {
+
+            return x / y;
+        }
     }
 }
